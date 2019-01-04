fe2f81413f578725247fc9794e1062e9c900572e
Dr.Snowbird <DrSnowbird@openkbs.org>
2019-01-04 01:04:06 -0500

    update wrapper_process.sh

--- wrapper_process.sh
+++ wrapper_process.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 
 set -e
-set -i
+
+printenv
 
 WAIT_SEC=60
-
-# /home/developer/denodo-platform-7.0/bin/denodo_platform.sh
 
 /home/developer/denodo-platform-7.0/bin/denodo_platform.sh
 
